7757af151586df52ae6b603ff544ce68412805ea
Julia-Albertim <juliaffalbertim@gmail.com>
2026-02-19 19:56:23 -0300

    ajuste table6 para receber com '.'

--- conversor_tabelas/processors/table6.py
+++ conversor_tabelas/processors/table6.py
@@ -1,9 +1,13 @@
 import re
-
 # 1 ABSORVENTE HOSPITALAR PACOTE - UNIDADE R$ 0,97
-
 def process(text):
-    # Padrão: Texto seguido de R$ e o valor
-    pattern = r'(.*?)\s+R\$\s*([\d\.]+,\d{2})'
+    pattern = r'(?:\d+\.\s*)?(.*?)\s+R\$\s*([\d\.]+,\d{2})'
     matches = re.findall(pattern, text)
-    return [{"Procedimento": m[0].strip().replace('.', ''), "Valor": f"R$ {m[1]}"} for m in matches]
+
+    return [
+        {
+            "Procedimento": m[0].strip(),
+            "Valor": f"R$ {m[1]}"
+        }
+        for m in matches
+    ]
